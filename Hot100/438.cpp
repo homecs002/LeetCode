a56b2575edc72515bf2163f647cf875a5fed6079
homecs002 <homecs002@gmail.com>
2025-07-27 21:33:22 +0800

    T438

--- Hot100/438.cpp
+++ Hot100/438.cpp
@@ -4,18 +4,40 @@
 using namespace std;
 
 
-class Solution{
+class Solution {
 public:
-    int strStr(vector<int> a){
+    vector<int> findAnagrams(string s, string p) {
+        int s_len = s.length();
+        int p_len = p.length();
+        
+        vector<int> ans;
 
-        return 1;
+        if(s_len < p_len) return ans; 
+
+        
+        vector<int>p_count(26);
+        for(int i = 0; i < p_len; i++){
+            p_count[p[i] - 'a'] += 1;
+        }
+
+        for (int i = 0; i < s_len; i++){
+            vector<int>s_count(26);
+            for (int j = 0; j < p_len && i + j < s_len; j++){
+                s_count[s[i + j] - 'a'] += 1;
+            }
+            if(s_count == p_count) ans.push_back(i);
+        }
+        return ans;
     }
 };
 
 int main(){
     // 定义好函数中的测试数据
-    vector<int> inp = {2, 3, 4};
+    string s = "cbaebabacd";
+    string p = "abc";
+    
     Solution solu;
+    vector<int> ans = solu.findAnagrams(s, p);
     
     return 0;
 }
