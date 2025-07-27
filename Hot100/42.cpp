ad170ab1ec29f75bad48fcb23f13bdbd1754b2bb
homecs002 <homecs002@gmail.com>
2025-07-27 20:04:47 +0800

    first

---
+++ Hot100/42.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+    public:
+        int trap(vector<int>& height) {
+            
+        }
+    };
+
+int main(){
+    // 定义好函数中的测试数据
+    vector<int> inp = {2, 3, 4};
+    Solution solu;
+    
+    return 0;
+}
